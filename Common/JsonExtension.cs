a403bbfab7a36e94a85b0e6220ce32d1833bf7b9
yitiaoxian <709008281@qq.com>
2020-12-07 16:15:04 +0800

    Common 提交

---
+++ Common/JsonExtension.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Common
+{
+    public static class JsonExtension
+    {
+        /// <summary>
+        /// 将字符串转为json
+        /// </summary>
+        /// <param name="Json"></param>
+        /// <returns></returns>
+        public static object ToJson(this string Json)
+        {
+            return Json == null ? null : JsonConvert.DeserializeObject(Json);
+        }
+        /// <summary>
+        /// object 转为json
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string ToJson(this object obj)
+        {
+            var thisConvert = new IsoDateTimeConverter
+            {
+                DateTimeFormat = "yyyy-MM-dd HH:mm:ss"
+            };
+            return JsonConvert.SerializeObject(obj,thisConvert);
+        }
+        /// <summary>
+        /// object转为json
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="datetimeformats"></param>
+        /// <returns></returns>
+        public static string ToJson(this object obj,string datetimeformats)
+        {
+            var timeConvert = new IsoDateTimeConverter { DateTimeFormat = datetimeformats };
+            return JsonConvert.SerializeObject(obj,timeConvert);
+        }
+        /// <summary>
+        /// json转为对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Json"></param>
+        /// <returns></returns>
+        public static T JsonToObject<T>(this string Json)
+        {
+
+            return Json == null ? default(T) : JsonConvert.DeserializeObject<T>(Json) ;
+        }
+        public static List<T> JsonToList<T>(this string Json)
+        {
+            return Json == null ? null : JsonConvert.DeserializeObject<List<T>>(Json);
+        }
+        /// <summary>
+        /// json串转为DataTable
+        /// </summary>
+        /// <param name="Json"></param>
+        /// <returns></returns>
+        public static DataTable JsonToTable(this string Json)
+        {
+            return Json == null ? null : JsonConvert.DeserializeObject<DataTable>(Json);
+        }
+        public static JObject JsonToJObject(this string Json)
+        {
+            return Json == null ? JObject.Parse("{}") : JObject.Parse(Json.Replace("&nbsp;", ""));
+        }
+        public static string DataTableToJson(this DataTable dt)
+        {
+            IsoDateTimeConverter timeConverter = new IsoDateTimeConverter { DateTimeFormat = "yyyy'-'MM'-'dd HH':'mm':'ss" };
+            return JsonConvert.SerializeObject(dt, Formatting.Indented, timeConverter);
+
+        }
+    }
+}
