52c731de99de9c2d3b4200263d9ec37587bf1872
yitiaoxian <709008281@qq.com>
2020-12-11 10:37:06 +0800

    服务启动类

---
+++ Service/Management.cs
@@ -0,0 +1,75 @@
+﻿using Opc.Ua;
+using Opc.Ua.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+    class Management
+    {
+        /// <summary>
+        /// 创建服务器实例
+        /// </summary>
+        public void CreateServerInstance()
+        {
+            try
+            {
+                var config = new ApplicationConfiguration()
+                {
+                    ApplicationName = "XOpcua",
+                    ApplicationUri = Utils.Format(@"urn:{0}:XOpcua", System.Net.Dns.GetHostName()),
+                    ApplicationType = ApplicationType.Server,
+                    ServerConfiguration = new ServerConfiguration()
+                    {
+                        BaseAddresses = { "opc.tcp://localhost:8020/", "https://localhost:8021/" },
+                        MinRequestThreadCount = 5,
+                        MaxRequestThreadCount = 100,
+                        MaxQueuedRequestCount = 200,
+                    },
+                    SecurityConfiguration = new SecurityConfiguration
+                    {
+                        ApplicationCertificate = new CertificateIdentifier { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\MachineDefault", SubjectName = Utils.Format(@"CN={0}, DC={1}", "XOpcua", System.Net.Dns.GetHostName()) },
+                        TrustedIssuerCertificates = new CertificateTrustList { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\UA Certificate Authorities" },
+                        TrustedPeerCertificates = new CertificateTrustList { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\UA Applications" },
+                        RejectedCertificateStore = new CertificateTrustList { StoreType = @"Directory", StorePath = @"%CommonApplicationData%\OPC Foundation\CertificateStores\RejectedCertificates" },
+                        AutoAcceptUntrustedCertificates = true,
+                        AddAppCertToTrustedStore = true
+                    },
+                    TransportConfigurations = new TransportConfigurationCollection(),
+                    TransportQuotas = new TransportQuotas { OperationTimeout = 15000 },
+                    ClientConfiguration = new ClientConfiguration { DefaultSessionTimeout = 60000 },
+                    TraceConfiguration = new TraceConfiguration()
+                };
+                config.Validate(ApplicationType.Server).GetAwaiter().GetResult();
+                if (config.SecurityConfiguration.AutoAcceptUntrustedCertificates)
+                {
+                    config.CertificateValidator.CertificateValidation += (s, e) => { e.Accept = (e.Error.StatusCode == StatusCodes.BadCertificateUntrusted); };
+                }
+
+                var application = new ApplicationInstance
+                {
+                    ApplicationName = "XOpcua",
+                    ApplicationType = ApplicationType.Server,
+                    ApplicationConfiguration = config
+                };
+                //验证
+                bool certOk = application.CheckApplicationInstanceCertificate(false, 0).Result;
+                if (!certOk)
+                {
+                    Console.WriteLine("证书验证失败!");
+                }
+
+                var dis = new DiscoveryServerBase();
+                // 启动服务器
+                application.Start(new XOpcuaServer()).Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("启动OPC-UA服务端触发异常:" + ex.Message);
+                Console.ResetColor();
+            }
+        }
+    }
+}
