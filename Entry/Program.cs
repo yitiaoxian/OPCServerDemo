cfaf50dc494e5c84990882f0287c03a8294f24c0
yitiaoxian <709008281@qq.com>
2020-12-16 21:37:48 +0800

    启动服务

--- Entry/Program.cs
+++ Entry/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using Service;
 
 namespace Entry
 {
     public class Program
     {
-        
+        static void Main(string[] args)
+        {
+            OpcuaManagement server = new OpcuaManagement();
+            server.CreateServerInstance();
+            Console.WriteLine("OPC UA服务已启动....");
+            Console.WriteLine();
+        }
     }
 }
